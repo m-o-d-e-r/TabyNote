84021925beac37213a44ec45f3283f3b540d6410
M_O_D_E_R <bogdan.chernenko.018@gmail.com>
2022-10-16 16:29:14 +0300

    first commit

---
+++ TabyNote/mainwindow.h
@@ -0,0 +1,51 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTextEdit>
+#include <QMessageBox>
+#include <QLabel>
+#include <QTextStream>
+#include <QString>
+#include <QFileDialog>
+#include <QFileInfo>
+#include <QFile>
+
+#include "texteditortab.h"
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    Ui::MainWindow *ui;
+    QLabel* currentFileNameLabel = new QLabel("File name: None");
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionOpen_triggered();    // on open file
+    void on_actionSave_as_triggered(); // on "save as" opration
+    void on_actionSave_triggered();    // on "save" operation
+
+    void on_actionNew_tab_triggered();              // create new tab
+    void on_actionClose_tab_triggered();            // close opened tab
+    void on_tabWidget_tabCloseRequested(int index); // close current tab by clicking criss
+    void on_tabWidget_currentChanged(int index);    // change titles while changing current tab
+
+    void on_actionQuit_triggered();
+
+    void on_actionAbout_triggered();
+
+private:
+    void __on_all_tabs_removed();
+    void __on_change_text_editor_callback();
+
+};
+#endif // MAINWINDOW_H
