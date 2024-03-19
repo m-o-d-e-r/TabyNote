73793bbf26e7582c5dacf2e2a0b8cf79585c4064
m-o-d-e-r <moder.mode.on@gmail.com>
2024-03-19 12:58:32 +0200

    created src folder

--- build-TabyNote-unknown-Debug/moc_mainwindow.cpp
+++ build-TabyNote-unknown-Debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../TabyNote/mainwindow.h"
+#include "../TabyNote/src/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
